ec76837ac558a982c21a47ad018feb51859a81c2
Tulus <tulus>
2020-01-03 12:57:57 +0700

    upd province

---
+++ province.php
@@ -0,0 +1,13 @@
+<?php
+error_reporting(0);
+ini_set('date.timezone', 'Asia/Jakarta');
+require('lib/bmkg.php');
+
+$province_id = substr(trim(strip_tags(@$_GET['province_id'])), 0, 2);
+$area_id = substr(trim(strip_tags(@$_GET['area_id'])), 0, 6);
+$bmkg = new BMKG("file_get_contents","xml");
+$data = $bmkg->province_list($province_id,$area_id);
+
+header('Content-Type: application/json');
+echo json_encode(array("stutus"=>"success","timestamp"=>time(),"data"=>$data), JSON_PRETTY_PRINT); 
+?>
